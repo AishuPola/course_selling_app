ccc0ae9f9a41aaa952ea3247b6ec3550f3b7c82d
AishuPola <aishwarya.pola@proclink.com>
2024-08-12 00:14:23 +0530

    created components

---
+++ src/app/adminprofile/adminprofile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminprofile',
+  standalone: true,
+  imports: [],
+  templateUrl: './adminprofile.component.html',
+  styleUrl: './adminprofile.component.scss'
+})
+export class AdminprofileComponent {
+
+}
