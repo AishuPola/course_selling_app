a8ff8953c54b7bc5506c4dedf42833d03bc6a3bf
AishuPola <aishwarya.pola@proclink.com>
2024-08-20 18:25:55 +0530

    removed login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
+
 import { AdminprofileComponent } from './adminprofile/adminprofile.component';
 import { AddcoursesComponent } from './addcourses/addcourses.component';
 import { UsersignupComponent } from './usersignup/usersignup.component';
 import { CoursesComponent } from './courses/courses.component';
-import { UserloginComponent } from './userlogin/userlogin.component';
+
 import { HomeComponent } from './home/home.component';
 import { UserhomeComponent } from './userhome/userhome.component';
 import { CoursedetailsComponent } from './coursedetails/coursedetails.component';
@@ -23,10 +23,7 @@ export const routes: Routes = [
     path: 'admin/signup',
     component: SignupComponent,
   },
-  {
-    path: 'admin/login',
-    component: LoginComponent,
-  },
+
   {
     path: 'adminprofile',
     component: AdminprofileComponent,
@@ -39,10 +36,7 @@ export const routes: Routes = [
     path: 'user/signup',
     component: UsersignupComponent,
   },
-  {
-    path: 'user/login',
-    component: UserloginComponent,
-  },
+
   {
     path: 'viewCourses',
     component: CoursesComponent,
